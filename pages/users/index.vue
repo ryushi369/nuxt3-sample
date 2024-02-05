ad10f26e453c4818cfd511a4e1157c1021f3bf5a
ryushi369 <ryushi.hatano@gmail.com>
2024-02-05 21:56:08 +0900

    feat: sample routing

---
+++ pages/users/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Users Top</h1>
+    <hr />
+    <NuxtLink to="/users/other">Other</NuxtLink>
+  </div>
+</template>
---
+++ pages/users/other.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Other</h1>
+    <hr />
+    <NuxtLink to="/">Users Top</NuxtLink>
+  </div>
+</template>
